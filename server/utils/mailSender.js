d115b226ace26350f06ba8dae70b920e742fd3be
RajsekharPanda <rajsekharpandawork@gmail.com>
2024-07-30 07:55:52 +0530

    Halfway added razorpay

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -7,20 +7,20 @@ const mailSender = async (email, title, body) => {
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
-      }
-    })
+      },
+    });
 
     let info = await transporter.sendMail({
-      from: `Study Notion`,
+      from: "StudyNotion || CodeHelp - by Babbar",
       to: `${email}`,
       subject: `${title}`,
-      html: `${body}`
+      html: `${body}`,
     });
     console.log(info);
     return info;
   } catch (error) {
     console.log(error.message);
   }
-}
+};
 
 module.exports = mailSender;
