429ed3bbc799f4fa05ead99c825796457b2a24f1
RajsekharPanda <rajsekharpandawork@gmail.com>
2024-08-28 21:18:56 +0530

    edit icon added

--- front-end/src/components/core/Dashboard/MyProfile.jsx
+++ front-end/src/components/core/Dashboard/MyProfile.jsx
@@ -1,3 +1,5 @@
+import { RiEditBoxLine } from "react-icons/ri"
+
 import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -33,7 +35,9 @@ const MyProfile = () => {
           onclick={() => {
             navigate("/dashboard/settings");
           }}
-        ></IconBtn>
+        >
+          <RiEditBoxLine />
+        </IconBtn>
       </div>
 
       {/* section 2 */}
@@ -45,7 +49,9 @@ const MyProfile = () => {
             onclick={() => {
               navigate("/dashboard/settings");
             }}
-          />
+          >
+            <RiEditBoxLine />
+          </IconBtn>
         </div>
         <p
           className={`${
@@ -61,42 +67,58 @@ const MyProfile = () => {
       {/* section 3 */}
       <div className="my-10 flex flex-col gap-y-10 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12">
         <div className="flex w-full items-center justify-between">
-          <p className="text-lg font-semibold text-richblack-5">Personal Details</p>
+          <p className="text-lg font-semibold text-richblack-5">
+            Personal Details
+          </p>
           <IconBtn
             text="Edit"
             onclick={() => {
               navigate("/dashboard/settings");
             }}
-          />
+          >
+            <RiEditBoxLine />
+          </IconBtn>
         </div>
-        <div  className="flex max-w-[500px] justify-between">
+        <div className="flex max-w-[500px] justify-between">
           <div className="flex flex-col gap-y-5">
             <div>
-            <p className="mb-2 text-sm text-richblack-600">First Name</p>
-            <p className="text-sm font-medium text-richblack-5">{user?.firstName}</p>
+              <p className="mb-2 text-sm text-richblack-600">First Name</p>
+              <p className="text-sm font-medium text-richblack-5">
+                {user?.firstName}
+              </p>
             </div>
           </div>
           <div>
             <p className="mb-2 text-sm text-richblack-600">Email</p>
-            <p className="text-sm font-medium text-richblack-5">{user?.email}</p>
-          </div>
-          <div>
-            <p className="mb-2 text-sm text-richblack-600">Gender</p>
-            <p className="text-sm font-medium text-richblack-5">{user?.additionalDetails?.gender ?? "Add Gender"}</p>
-          </div>
-          <div>
-            <p className="mb-2 text-sm text-richblack-600">Last Name</p>
-            <p className="text-sm font-medium text-richblack-5">{user?.lastName}</p>
-          </div>
-          <div>
-            <p className="mb-2 text-sm text-richblack-600">Phone Number</p>
             <p className="text-sm font-medium text-richblack-5">
-              {user?.additionalDetails?.contactNumber ?? "Add Contact Number"}
+              {user?.email}
             </p>
           </div>
           <div>
-            <p className="mb-2 text-sm text-richblack-600">Date of Birth</p>
-            <p className="text-sm font-medium text-richblack-5">{user?.additionalDetails?.dateOfBirth ?? "Add Date of Birth"}</p>
+            <p className="mb-2 text-sm text-richblack-600">Gender</p>
+            <p className="text-sm font-medium text-richblack-5">
+              {user?.additionalDetails?.gender ?? "Add Gender"}
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-5">
+            <div>
+              <p className="mb-2 text-sm text-richblack-600">Last Name</p>
+              <p className="text-sm font-medium text-richblack-5">
+                {user?.lastName}
+              </p>
+            </div>
+            <div>
+              <p className="mb-2 text-sm text-richblack-600">Phone Number</p>
+              <p className="text-sm font-medium text-richblack-5">
+                {user?.additionalDetails?.contactNumber ?? "Add Contact Number"}
+              </p>
+            </div>
+            <div>
+              <p className="mb-2 text-sm text-richblack-600">Date of Birth</p>
+              <p className="text-sm font-medium text-richblack-5">
+                {user?.additionalDetails?.dateOfBirth ?? "Add Date of Birth"}
+              </p>
+            </div>
           </div>
         </div>
       </div>
