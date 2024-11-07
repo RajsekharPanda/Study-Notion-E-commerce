53e607ca161a2d16c10a5dbe57a98708413412dc
RajsekharPanda <rajsekharpandawork@gmail.com>
2024-11-07 06:00:45 +0530

    no changes made

--- front-end/src/services/apiconnector.js
+++ front-end/src/services/apiconnector.js
@@ -5,7 +5,7 @@ export const axiosInstance = axios.create({});
 export const apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
     method: `${method}`,
-    url: `https://study-notion-ed-tech-my5r.onrender.com/api/v1/auth/sendotp`,
+    url: `${url}`,
     data: bodyData ? bodyData : null,
     headers: headers ? headers : null,
     params: params ? params : null,
