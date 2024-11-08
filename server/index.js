02616dd93cddadee6d39fe24550200a69e319084
RajsekharPanda <rajsekharpandawork@gmail.com>
2024-11-08 14:16:56 +0530

    testin bug

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(
   cors({
     origin: '*',
     credentials: true,
-    methods: 'GET,POST', // Allowed HTTP methods
+    methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
     allowedHeaders: 'Content-Type,Authorization',
   })
 );
